501033164945610f739a6ab0199b938faea6f25a
sabinasim <85550957+sabinasim@users.noreply.github.com>
2022-08-16 16:49:40 +0800

    Add files via upload
    Basic Segment integration with Braze web SDK

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿let userId, customEvent;
+
+//change user
+document.getElementById("submit_userid").addEventListener("click", function () {
+    userId = document.getElementById("change_userid").value;
+    braze.changeUser(userId);
+
+    alert("Submitted " + userId);
+    analytics.track('First Visit');
+});
+
+//log custom attribtute
+document.getElementById("submit_customevent").addEventListener("click", function () {
+    customEvent = document.getElementById("add_customevent").value;
+    analytics.track(customEvent);
+
+    alert("Submitted custom event to Segment: " + customEvent);
+});
